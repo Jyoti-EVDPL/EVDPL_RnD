91707f3ace8c218eb7d060e6b405c88454b73997
Jyoti-EVDPL <121219411+Jyoti-EVDPL@users.noreply.github.com>
2023-12-08 11:38:32 +0530

    initial commit in playwright branch

---
+++ oauth-react/src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import { jwtDecode } from "jwt-decode"
+import { useState, useEffect } from "react"
+
+function App() {
+  const [user, setUser] = useState({});
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+    var userObject = jwtDecode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+  function handleSignOut(event) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "312861415918-cg5j5hqpf35u2vop85s33ik9dittn0kf.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large" }
+    );
+  }, []);
+  // If we have no user: sign in button
+  // if we have a user: show the log out button
+
+  return (
+    <div className="App">
+      <div id="signInDiv"></div>
+      {Object.keys(user).length !== 0 &&
+        <button onClick={(e) => handleSignOut(e)}>Sign Out</button>
+      }
+      user && <div>
+        <img src={user.picture} alt='img'></img>
+        <h3>{user.name}</h3>
+      </div>
+    </div>
+  );
+  // return (
+  //   <>
+  //     <h1>Hello there:User Pleasse Signin</h1>
+  //     <button>Click to register</button>
+
+
+  //   </>
+  // );
+}
+
+export default App;
